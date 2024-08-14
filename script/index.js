0938fa8db2aac91d223e7dfbb44f859b371f10df
JacqueADev <jacquelineadev@gmail.com>
2024-08-14 12:13:22 -0300

    Implantação da API de planilhas do google
    captura de dados no campo de cadastro

--- script/index.js
+++ script/index.js
@@ -3,3 +3,32 @@ document.getElementById('abrirFormCadastro').addEventListener('click', function(
     var formulario = document.getElementById('formularioDeCadastro');
     formulario.classList.toggle('show'); // Alterna a classe 'show'
   });
+
+  document.getElementById('formulario-cadastro').addEventListener('submit', function(e) {
+    e.preventDefault();
+    
+    var nome = document.getElementById('nome').value;
+    var email = document.getElementById('email').value;
+    var senha = document.getElementById('senha').value;
+  
+    var data = {
+      "nome": nome,
+      "email": email,
+      "senha": senha
+    };
+  
+    fetch('https://script.google.com/macros/s/AKfycbyqABS_UrtkHDOdDFmiZhUYl20fyWHm9TeKL3LeupgXjrUUBMLo_qX41gp440pj1P91/exec', {
+      method: 'POST',
+      mode: 'no-cors',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+    .then(response => {
+      console.log('Success:', response);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+  });
