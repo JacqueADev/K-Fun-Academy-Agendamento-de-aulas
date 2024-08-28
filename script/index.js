aa3f7a1acf533f454b715b6e3e53f6d204b442f6
JacqueADev <jacquelineadev@gmail.com>
2024-08-28 11:47:57 -0300

    Mudanças no login

--- script/index.js
+++ script/index.js
@@ -1,11 +1,17 @@
-// Abrir formulário de cadastro
+// Evento de clique para abrir o formulário de cadastro
 document.getElementById('abrirFormCadastro').addEventListener('click', function(event) {
-  event.preventDefault(); // Previne o comportamento do link
-  var formulario = document.getElementById('formularioDeCadastro');
-  formulario.classList.toggle('show'); // Alterna a classe 'show'
+  event.preventDefault(); // Evita que o link execute a ação padrão de recarregar a página
+  var formCadastro = document.getElementById('formularioDeCadastro');
+  
+  // Alterna entre mostrar e esconder o formulário de cadastro
+  if (formCadastro.classList.contains('show')) {
+    formCadastro.classList.remove('show');
+  } else {
+    formCadastro.classList.add('show');
+  }
 });
 
-// Envio de dados de cadastro
+
 document.getElementById('formulario-cadastro').addEventListener('submit', function(e) {
   e.preventDefault();
   
@@ -16,53 +22,89 @@ document.getElementById('formulario-cadastro').addEventListener('submit', functi
   var data = {
     "nome": nome,
     "email": email,
-    "senha": senha
+    "senha": senha,
+    "action": "store" // Adiciona um campo de ação para o Google Apps Script
   };
 
-  fetch('https://script.google.com/macros/s/AKfycbyqABS_UrtkHDOdDFmiZhUYl20fyWHm9TeKL3LeupgXjrUUBMLo_qX41gp440pj1P91/exec', {
+  fetch('https://script.google.com/macros/s/AKfycbwDCB2BT4EdB4YUw3Vz4SxNF1ba2WlAKyW1zL4_Igg7uZDPXuNN8iXldFv7K9fsgpTr/exec', {
     method: 'POST',
-    mode: 'no-cors',
     headers: {
       'Content-Type': 'application/json'
     },
     body: JSON.stringify(data)
   })
   .then(response => {
-    console.log('Success:', response);
+    if (response.ok) {
+      // Exibe o alerta se a resposta for certa
+      alert('Cadastro realizado com sucesso!');
+      document.getElementById('formulario-cadastro').reset(); // Limpa o formulário
+    } else {
+      // exibe um alerta de erro
+      alert('Ocorreu um erro ao realizar o cadastro. Tente novamente.');
+    }
   })
   .catch((error) => {
     console.error('Error:', error);
+    alert('Ocorreu um erro ao realizar o cadastro. Tente novamente.');
   });
 });
 
-// Função para buscar dados da planilha Google Sheets
-function getSheetData() {
-  var spreadsheetId = '18mK5b2F-WCk4ImuPzUhj6OO4HMNG_fHa71wlyVSGkiU'; // Substitua com o ID da sua planilha
-  var range = "Dados!B1:C100"; // Defina o intervalo que você deseja buscar
-  var apiKey = 'AIzaSyDt2KGKqEzruw2lqgL5BH58jH7NW55TAng'; // Substitua com sua API key
 
-  fetch(`https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${range}?key=${apiKey}`)
+
+
+
+// Buscando dados da planilha
+function getSheetData(emailLogin, senhaLogin) {
+  var spreadsheetId = '18mK5b2F-WCk4ImuPzUhj6OO4HMNG_fHa71wlyVSGkiU'; //ID da planilha
+  var range = "Dados!B1:C100"; // Defina o intervalo 
+  var apiKey = 'AIzaSyDt2KGKqEzruw2lqgL5BH58jH7NW55TAng'; // Substituir com sua API key
+
+  return fetch(`https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${range}?key=${apiKey}`)
     .then((response) => response.json())
     .then((data) => {
       const arrayDeObjetos = data.values.map((linha) => {
-        const [name, email, password] = linha;
-        return { name, email, password };
+        const [ email, password] = linha;
+        return { email, password };
       });
-      console.log(arrayDeObjetos); // Exibe os dados da planilha no console
+      return arrayDeObjetos;
     })
     .catch((error) => {
       console.error('Erro ao buscar dados da planilha:', error);
+      return null;
     });
 }
 
-// Evento de clique para o botão de login
+// clique para o botão de login
 document.getElementById('loginButton').addEventListener('click', function() {
   var emailLogin = document.getElementById('email-login').value;
   var senhaLogin = document.getElementById('senha-login').value;
 
-  console.log('Email:', emailLogin);
-  console.log('Senha:', senhaLogin);
+  if (emailLogin === "" || senhaLogin === "") {
+    alert('Por favor, preencha todos os campos!');
+    return;
+  }
 
-  // Chama a função para buscar dados da planilha após a autenticação
-  getSheetData();
+  getSheetData(emailLogin, senhaLogin).then(arrayDeObjetos => {
+    if (arrayDeObjetos) {
+      // Verifica se os dados preenchidos com os da planilha
+      const usuarioEncontrado = arrayDeObjetos.find(objeto => objeto.email === emailLogin && objeto.password === senhaLogin);
+
+      if (usuarioEncontrado) {
+        // Se tá certo exibe a seção "mural-aviso"
+        document.querySelector('section.mural-aviso').style.display = 'block';
+
+        // Oculta todas as outras seções, fora o header
+        document.querySelectorAll('section:not(.mural-aviso), div.cadastro-link').forEach(section => {
+          section.style.display = 'none';
+        });
+
+      } else {
+        // Se tá errado
+        alert('Email ou senha incorretos. Tente novamente.');
+      }
+    } else {
+      alert('Erro ao verificar os dados. Tente novamente mais tarde.');
+    }
+  });
 });
+
